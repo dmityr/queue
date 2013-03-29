9aa912dd4971b299c466568cc5559e15819c73c2
Dmitry Romaniuk <dmityr.ro@gmail.com>
2013-03-29 13:07:57 +0300

    validate data before save, accept only Task

--- queue.rb
+++ queue.rb
@@ -10,12 +10,19 @@ class Queue
     @counter = 0
   end
 
+  # push new task in the pool
   def push task
+    raise QueueError, "Incorrect data type" unless valid? task
   end
 
   def get_task finish_at
   end
 
   def pop
+  end
+
+  private
+  def valid? task
+    task.class == Task
   end
 end
