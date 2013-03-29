bc999f7ad23f0dfa557e21a80fec0c4f84f2e762
Dmitry Romaniuk <dmityr.ro@gmail.com>
2013-03-29 13:50:24 +0300

    accept only Time type for new tasks

--- task.rb
+++ task.rb
@@ -5,7 +5,7 @@ class Task
   attr_accessor :finish_at, :description
 
   def initialize finish_at, description
-    raise TaskException, "Incorrect finish at type, accept only Time"
+    raise TaskException, "Incorrect finish at type, accept only Time" unless finish_at.is_a?(Time)
 
     @finish_at = finish_at
     @description = description
